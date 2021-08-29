d965628fc10935c51e8d1626878a30ea9cd0b33a
Justin Lam <justin.kh.lam@gmail.com>
2021-08-29 21:43:35 +0000

    re-create repo

---
+++ express_server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+
+// PORT
+const PORT = 8080; 
+// default port 8080
+
+app.set("view engine", "ejs");
+
+// FEED DATA
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+// GET ROUTE HANDLERS
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL /* What goes here? */ };
+  res.render("urls_show", templateVars);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+// PORT LISTENER
+
+app.listen(PORT, () => {
+  console.log(`TinyApp listening on port ${PORT}!`);
+});
